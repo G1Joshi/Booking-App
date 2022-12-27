247d7c69876d569b318b2407b8604cf192499ce3
Jeevan Joshi <joshijeevan98@gmail.com>
2022-12-27 13:34:19 +0530

    handle exceptions in middleware

--- booking_backend/lib/middleware/db_connection_middleware.dart
+++ booking_backend/lib/middleware/db_connection_middleware.dart
@@ -1,13 +1,26 @@
+import 'dart:io';
+
 import 'package:booking_backend/database/connection.dart';
+import 'package:booking_backend/models/general_response.dart';
 import 'package:dart_frog/dart_frog.dart';
 
 Middleware dbConnection(Connection connection) {
   return (handler) {
     return (context) async {
-      await connection.connect;
-      final response = await handler(context);
-      await connection.disconnect;
-      return response;
+      try {
+        await connection.connect;
+        final response = await handler(context);
+        await connection.disconnect;
+        return response;
+      } catch (e) {
+        return Response.json(
+          statusCode: HttpStatus.badRequest,
+          body: GeneralResponse(
+            status: false,
+            message: '$e',
+          ),
+        );
+      }
     };
   };
 }
