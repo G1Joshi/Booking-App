5468a91fe77c29ee29830a81d608446098918573
Jeevan Joshi <joshijeevan98@gmail.com>
2026-02-07 18:29:19 +0530

    setup workspace

--- apps/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import google_sign_in_ios
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
